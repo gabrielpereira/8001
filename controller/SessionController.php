161fb941f781477cfe9ee274799f4c7be03125e8
Renard <renard.sebastian@gmail.com>
2016-06-12 19:42:48 -0300

    Login via Ajax, sessions e pá. Acho que tá bonito!

---
+++ controller/SessionController.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Renard
+ * Date: 12/06/2016
+ * Time: 18:40
+ */
+
+class SessionController {
+
+}
---
+++ view/logout.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Renard
+ * Date: 12/06/2016
+ * Time: 19:01
+ */
